9e15533ec72ad565dc48fd858a0265db5aaec2b5
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-02-27 21:28:14 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "no-cond-assign": [
+            "error",
+            "always"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ]
+    }
+};

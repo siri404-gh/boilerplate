15728559cf85b6784e5ce274cf2dfe9595ab79b0
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-03-01 11:49:15 +0000

    updates to build

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const {
-  entry, dist, outputFileName, manifestPlugin, _module, resolveExtensions, stats,
+  entry, dist, outputFileName, _module, resolveExtensions, stats,
 } = require('./webpack.utils');
 
 module.exports = {
@@ -15,9 +15,7 @@ module.exports = {
     extensions: resolveExtensions,
     modules: [path.join(__dirname, 'components'), 'node_modules'],
   },
-  // plugins: [
-  //   manifestPlugin,
-  // ],
+  plugins: [],
   module: _module,
   stats,
 };
